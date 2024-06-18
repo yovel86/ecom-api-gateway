eb5caebc315a1457e865863bc6e22abc34d6fdc5
Yovel <yovel.trichy@gmail.com>
2024-06-18 15:46:59 +0530

    Implemented Api Gateway

---
+++ src/main/java/com/projects/ecom_api_gateway/EcomApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.projects.ecom_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/ecom_api_gateway/EcomApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ecom_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
